c9d65446b44966db43b19862f5fca798a1a7356e
Katugwa Nathaniel <nkatugwa@gmail.com>
2026-07-16 19:25:48 +0100

    feat(database): create initial users table

---
+++ backend/app/models/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy.sql import func
+
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    first_name = Column(String, nullable=False)
+
+    last_name = Column(String, nullable=False)
+
+    email = Column(String, unique=True, nullable=False, index=True)
+
+    hashed_password = Column(String, nullable=False)
+
+    is_active = Column(Boolean, default=True)
+
+    is_verified = Column(Boolean, default=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
